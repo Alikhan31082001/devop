af9fd660130b771e62e32c3b01fa7c1e1fa8591a
Alikhan31082001 <alihan.ismagulov05@gmail.com>
2023-11-29 14:22:25 +0600

    yutg

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+import unittest
+class Test(TestCase):
+    def test_1(self):
+        self.assertEqual(1, 1)
 
-# Create your tests here.
+
+
+
+class DoszhanTestCase(unittest.TestCase):
+    def test_numbers(self):
+        self.assertEqual(3 + 4, 7)
+        self.assertEqual(3 + 4, 7)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+class MyDoszhan2TestCase(unittest.TestCase):
+    def test_strings(self):
+        self.assertEqual("hello" + " " + "world", "hello world")
+        self.assertEqual("hello" + " " + "world", "hello world")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
